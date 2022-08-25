6036cd4d79e5be0b1e2f157e08927b7edd0cf870
iamhappymonk <bhavish@happymonk.co>
2022-08-25 23:51:43 +0530

    correcting the frwmes path

--- full_code/full_pipeline_camera.c
+++ full_code/full_pipeline_camera.c
@@ -148,9 +148,9 @@ static gboolean hls_server_device(char *file_name, char *file_url, char *file_pa
 
     gchar *gst_str1, *gst_str2, *gst_str3;
 
-    gst_str1 = g_strdup_printf("rtspsrc location=%s ! rtph264depay ! avdec_h264 ! clockoverlay ! videoconvert ! videoscale ! video/x-raw,width=640, height=360 ! x264enc bitrate=512 ! hlssink2 playlist-root=http://127.0.0.1:8080 playlist-location=%s/%s.m3u8 location=%s", file_url, file_path, file_name, file_path);
+    gst_str1 = g_strdup_printf("rtspsrc location=%s ! rtph264depay ! avdec_h264 ! clockoverlay ! videoconvert ! videoscale ! video/x-raw,width=640, height=360 ! x264enc bitrate=512 ! hlssink2 playlist-root=http://127.0.0.1:8080/stream%s playlist-location=%s/%s.m3u8 location=%s", file_url, file_name, file_path, file_name, file_path);
 
-    gst_str2 = "/segment.%05d.ts target-duration=15  max-files=15 playlist-length=30";
+    gst_str2 = "/segment.%05d.ts target-duration=30  max-files=30 playlist-length=30";
 
     gst_str3 = (char *)malloc(1 + strlen(gst_str1) + strlen(gst_str2));
 
@@ -657,7 +657,7 @@ int main(int argc, gchar *argv[])
     //     s = natsConnection_QueueSubscribe(&sub, conn, subject, queueGroup, onMsg, (void *)&done);
     // }
 
-    for (int i = 3; i <= 3; i++)
+    for (int i = 1; i <= 7; i++)
     {
         char *new_path = g_strdup_printf("rtsp://happymonk:admin123@192.168.1.10%d:554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif", i);
         char *new_id = g_strdup_printf("%d", i);
